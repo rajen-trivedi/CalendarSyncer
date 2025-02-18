2959db34c3176a6fe902c83a2659884e4a23a711
Rajen Trivedi <rbtrivedi2704@gmail.com>
2025-02-18 23:49:54 +0530

    Resolve the issue: > Task :app:checkDebugAarMetadata FAILED
    Solution: Remove the androidx.activity library and update the Kotlin version to 1.13.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
